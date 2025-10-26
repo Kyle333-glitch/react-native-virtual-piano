7fa43586288ab0e4bc593a4e46c9c2171b3c1b4b
Kyle333-glitch <kyleq217@gmail.com>
2025-10-25 19:56:26 -0700

    refactor(piano): migrate Piano.tsx to React Native with typed state/handlers

--- lib/react-piano/Piano.tsx
+++ lib/react-piano/Piano.tsx
@@ -1,36 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import difference from 'lodash.difference';
 import ControlledPiano from './ControlledPiano';
-import Keyboard from './Keyboard';
 
-class Piano extends React.Component {
-  static propTypes = {
-    noteRange: PropTypes.object.isRequired,
-    activeNotes: PropTypes.arrayOf(PropTypes.number.isRequired),
-    playNote: PropTypes.func.isRequired,
-    stopNote: PropTypes.func.isRequired,
-    onPlayNoteInput: PropTypes.func,
-    onStopNoteInput: PropTypes.func,
-    renderNoteLabel: PropTypes.func,
-    className: PropTypes.string,
-    disabled: PropTypes.bool,
-    width: PropTypes.number,
-    keyWidthToHeight: PropTypes.number,
-    keyboardShortcuts: PropTypes.arrayOf(
-      PropTypes.shape({
-        key: PropTypes.string.isRequired,
-        midiNumber: PropTypes.number.isRequired,
-      }),
-    ),
+type PianoProps = {
+  activeNotes?: ReadonlyArray<number>;
+  onPlayNoteInput?: (midi: number, ctx: { prevActiveNotes: number[] }) => void;
+  onStopNoteInput?: (midi: number, ctx: { prevActiveNotes: number[] }) => void;
+  // add other props want to support:
+  // noteRange: { first: number; last: number };
+  // playNote: (midi: number) => void;
+  // stopNote: (midi: number) => void;
+  // style?: StyleProp<ViewStyle>;
+};
+
+type PianoState = {
+  activeNotes: number[];
+};
+
+class Piano extends React.Component<PianoProps, PianoState> {
+
+  state: PianoState = {
+    activeNotes: [...(this.props.activeNotes ?? [])],
   };
 
-  state = {
-    activeNotes: this.props.activeNotes || [],
-  };
-
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps: PianoProps) {
     // Make activeNotes "controllable" by using internal
     // state by default, but allowing prop overrides.
     if (
@@ -38,12 +30,12 @@ class Piano extends React.Component {
       this.state.activeNotes !== this.props.activeNotes
     ) {
       this.setState({
-        activeNotes: this.props.activeNotes || [],
+        activeNotes: [...(this.props.activeNotes ?? [])],
       });
     }
   }
 
-  handlePlayNoteInput = (midiNumber) => {
+  handlePlayNoteInput = (midiNumber: number) => {
     this.setState((prevState) => {
       // Need to be handled inside setState in order to set prevActiveNotes without
       // race conditions.
@@ -61,12 +53,12 @@ class Piano extends React.Component {
     });
   };
 
-  handleStopNoteInput = (midiNumber) => {
+  handleStopNoteInput = (midiNumber: number) => {
     this.setState((prevState) => {
       // Need to be handled inside setState in order to set prevActiveNotes without
       // race conditions.
       if (this.props.onStopNoteInput) {
-        this.props.onStopNoteInput(midiNumber, { prevActiveNotes: this.state.activeNotes });
+        this.props.onStopNoteInput(midiNumber, { prevActiveNotes: prevState.activeNotes });
       }
       return {
         activeNotes: prevState.activeNotes.filter((note) => midiNumber !== note),
