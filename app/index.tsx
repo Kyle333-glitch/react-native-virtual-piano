fce9790e5b18f09469af543a884286ff7b476c7e
Kyle333-glitch <203567887+Kyle333-glitch@users.noreply.github.com>
2025-10-31 05:54:47 -0700

    feat(homeScreen): render Piano in index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"
+
+import { Piano, MidiNumbers } from "../lib/react-piano";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Piano
+        noteRange={{
+          first: MidiNumbers.fromNote('c4'),
+          last: MidiNumbers.fromNote('c5'),
+        }}
+        onNoteOn={(midi, ctx) => console.log('Note on:', midi, ctx)}
+        onNoteOff={(midi, ctx) => console.log('Note off:', midi, ctx)}
+      />
+    </SafeAreaView>
   );
 }
