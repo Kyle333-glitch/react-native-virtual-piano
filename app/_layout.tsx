782f7972f7c3cdddae02aef27fbf176fa0e9968d
Kyle333-glitch <203567887+Kyle333-glitch@users.noreply.github.com>
2025-11-02 14:38:07 -0800

    feat(navigation): replace empty Stack with Drawer navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,53 @@
-import { Stack } from "expo-router";
+import { Drawer } from "expo-router/drawer";
+import NativeIcon from "../components/NativeIcons"
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <Drawer
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "blue",
+          width: 240,
+          height: "100%",
+        },
+        drawerPosition: "right",
+        drawerType: "front",
+        drawerActiveTintColor: "white",
+        drawerInactiveTintColor: "#ccc",
+        headerStyle: { backgroundColor: "#fff"},
+        headerTintColor: "black",
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          drawerLabel: "Piano",
+          title: "Piano",
+          drawerIcon: ({ color, size }) => (
+            <NativeIcon name="piano" color={color} size={size}/>
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="settings"
+        options={{
+          drawerLabel: "Settings",
+          title: "Settings",
+          drawerIcon: ({ color, size }) => (
+            <NativeIcon name="settings" color={color} size={size}/>
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="appearance"
+        options={{
+          drawerLabel: "Appearance",
+          title: "Appearance",
+          drawerIcon: ({ color, size }) => (
+            <NativeIcon name="palette" color={color} size={size}/>
+          ),
+        }}
+      />
+    </Drawer>
+  );
 }
