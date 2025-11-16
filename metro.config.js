0bf3a95dc71f059affab3eadce7f61d8756a6101
Kyle333-glitch <203567887+Kyle333-glitch@users.noreply.github.com>
2025-11-16 11:49:11 -0800

    fix(metro.config.js): add metro config to support .flac and .m4a

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts = [
+  ...config.resolver.assetExts,
+  "flac",
+  "m4a",
+];
+
+module.exports = config;
